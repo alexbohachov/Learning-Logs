06ed84f10f9596828aa841b6d3d1f6394dacf01c
Alex <alexbogachev93@protonmail.com>
2022-07-03 23:49:17 +0300

    new_topic, new_entry, edit_entry

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('topic/', views.topics, name='topics'),
     path('topics/<int:topic_id>', views.topic, name='topic'),
+    path('new_topic/', views.new_topic, name='new_topic'),
+    #The page for adding a new entry.
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    #The page for editing an entry.
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
     ]
